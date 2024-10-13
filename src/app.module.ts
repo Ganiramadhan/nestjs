b0c05699e911b9bbb3036098568054e9109233fb
ganiramadhan <ganiramadhan22@gmail.com>
2024-10-13 19:48:27 +0700

    update root

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,21 @@ import { ApiController } from './api/api.controller';
 import { ApiService } from './api/api.service';
 import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
-
+import { AppController } from './app.controller'; 
+import { AppService } from './app.service';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     HttpModule,
-    PostModule, 
+    PostModule,
   ],
-  controllers: [ApiController],
-  providers: [ApiService],
+  controllers: [
+    ApiController,
+    AppController, 
+  ],
+  providers: [
+    ApiService,
+    AppService,
+  ],
 })
 export class AppModule {}
